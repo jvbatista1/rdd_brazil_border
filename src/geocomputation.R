e1de88ee617c946c30251d27744c74ba5d34759d
jvbatista1 <batistajvictor@gmail.com>
2024-12-12 12:01:50 -0300

    sedes

---
+++ src/geocomputation.R
@@ -0,0 +1,212 @@
+dropbox <- "c:/Users/victor/dropbox/DISSERTACAO"
+
+#### Meu exemplo
+library(sf)
+library(dplyr)
+fronteira <- read_sf(file.path(dropbox,"Fronteira/Faixa_de_Fronteira_por_UF_2022.shp")) %>%
+  st_transform("WGS84") |> 
+  rename("id_uf" = "CD_UF",
+         "nome_uf" = "NM_UF",
+         'sigla_uf' = "SIGLA_UF",
+         "nome_regiao" = "NM_REGIAO",
+         "area_uf" = "AREA_KM2",
+         "area_integrada" = "AREA_INT",
+         "porcentagem_integrada" = "PORC_INT")
+fronteira
+
+#Uniformiza a faixa de fronteira como uma única região
+linha_fronteira <- fronteira %>%
+  mutate(pais = "BR") %>% 
+  group_by(pais) %>% 
+  summarise()
+
+#Carrega o sf dos municípios da faixa de fronteira
+municipios_fronteira <- read_sf(file.path(dropbox,"Municipios_Fronteira/Municipios_Faixa_Fronteira_2022.shp")) %>%
+  st_transform("WGS84") |> 
+  rename("id_municipio" = "CD_MUN",
+         "nome" = "NM_MUN",
+         "id_uf" = "CD_UF",
+         "nome_uf" = "NM_UF",
+         'sigla_uf' = "SIGLA_UF",
+         "nome_regiao" = "NM_REGIAO",
+         "area_municipio" = "AREA_TOT",
+         "area_integrada" = "AREA_INT",
+         "porcentagem_integrada" = "PORC_INT") |> 
+  mutate(gemea = !(is.na(CID_GEMEA)), .keep = "unused", .after = "porcentagem_integrada")
+
+municipios_fronteira
+
+#Carrega sf dos municípios brasileiros
+library(geobr)
+municipios <- read_municipality(year=2020, showProgress = T, simplified = T) |> 
+  st_transform("WGS84") |> 
+  rename("id_municipio" = "code_muni",
+         "nome" = "name_muni",
+         "id_uf" = "code_state",
+         "nome_uf" = "name_state",
+         'sigla_uf' = "abbrev_state",
+         "nome_regiao" = "name_region",
+         "id_regiao" = "code_region")|> 
+  mutate(id_municipio = as.character(id_municipio))
+
+# Estabelece a nova proposta de faixa de fronteira
+linha_fronteira_300km <- st_buffer(linha_fronteira, dist = 150000)
+
+# Verifica municípios que passam a pertencer à região
+# Adiciona variável de intercessão
+municipios$inter <- st_intersects(municipios, linha_fronteira_300km, sparse = F)
+
+# Adiciona variável de pertencimento à fronteira original
+municipios <- municipios %>%
+  mutate(fronteira = ifelse(id_municipio %in% municipios_fronteira$id_municipio, 1, 0))
+
+# Cria tratamento e controle
+df <- municipios |>
+  #filtra os municípios na nova faixa
+  filter(inter == T) |> 
+  # cria o grupo de tratamento e controle
+  mutate(treated = ifelse(id_municipio %in% municipios_fronteira$id_municipio, 1, 0),
+         groups = ifelse(treated == 1, "treatment", "control"),
+         # cria os arcos
+         arcos = case_when(sigla_uf %in% c("AP", "PA", "AM", "AC", "RR") ~ "Arco Norte",
+                           sigla_uf %in% c("RO", "MS", "MT") ~ "Arco Central",
+                           sigla_uf %in% c("PR", "SC", "RS") ~ "Arco Sul",
+                           sigla_uf %in% c("SP") ~ "Arco Sudeste")) |> 
+  # exclui a variável classificatória. as recém criadas a substituem
+  dplyr::select(-inter, -fronteira, -treated) 
+  
+
+# prepara a tabela da fronteira para mergir com a df principal (municipios)
+t <- municipios_fronteira |> 
+  # seleciona colunas de interesse
+  select("id_municipio", "area_municipio", "area_integrada", "porcentagem_integrada", "gemea")
+
+# remove o componente gráfico
+st_geometry(t) <- NULL
+
+# realiza o join
+df <- dplyr::left_join(df, t, by = "id_municipio")
+
+df
+rm(t)
+
+#Carrega o sf dos países da América do Sul
+america <- st_read(file.path(dropbox,"America/South_America.shp")) %>% 
+  st_transform("WGS84")
+
+# prepara para juntar demais países da américa do sul na base de municípios
+# Remove regiões sem fronteira com o br
+paises_fronteira <- america %>%
+  filter(COUNTRY %in% c("French Guiana (France)", "Suriname", "Guyana", "Venezuela", 
+                        "Colombia", "Peru", "Bolivia", "Paraguay", "Argentina", "Uruguay"))
+
+# verifica interseções
+a <- st_intersects(df, paises_fronteira, sparse = FALSE)
+
+# renomeia colunas e cria variáveis dummy
+a <- as.data.frame(a) |> 
+  rename("Argentina" = "V1",
+         "Bolivia" = "V2",
+         "Colombia" = "V3",
+         "French_Guiana" = "V4",
+         "Guyana" = "V5",
+         "Suriname" = "V6",
+         "Paraguay" = "V7",
+         "Peru" = "V8",
+         "Uruguay" = "V9",
+         "Venezuela" = "V10") |> 
+  mutate(Argentina = ifelse(Argentina == T, 1, 0),
+         Bolivia = ifelse(Bolivia == T, 1, 0),
+         Colombia = ifelse(Colombia == T, 1, 0),
+         French_Guiana = ifelse(French_Guiana == T, 1, 0),
+         Guyana = ifelse(Guyana == T, 1, 0),
+         Suriname = ifelse(Suriname == T, 1, 0),
+         Paraguay = ifelse(Paraguay == T, 1, 0),
+         Peru = ifelse(Peru == T, 1, 0),
+         Uruguay = ifelse(Uruguay == T, 1, 0),
+         Venezuela = ifelse(Venezuela == T, 1, 0))
+
+df <- cbind(df, a)
+rm(a)
+
+library(units)
+
+brasil <- america %>%
+  filter(COUNTRY == "Brazil")
+
+sede_municipios <- read_municipal_seat(year=2010, showProgress = T) %>%
+  st_transform("WGS84") |> 
+  rename("id_municipio" = "code_muni",
+         "nome" = "name_muni",
+         "id_uf" = "code_state",
+         'sigla_uf' = "abbrev_state",
+         "nome_regiao" = "name_region",
+         "id_regiao" = "code_region",
+         "ano" = "year") |> 
+  mutate(id_municipio = as.character(id_municipio))
+
+t <- select(df, id_municipio)
+st_geometry(t) <- NULL
+
+sede_municipios <- sede_municipios |> 
+  semi_join(t, by="id_municipio")
+
+rm(t)
+
+# Criar um buffer de 50 metros para dentro do polígono do Brasil na fronteira com esses países
+buffer_fronteira <- st_buffer(paises_fronteira, dist = 50)
+
+# Definir a linha da fronteira terrestre do Brasil com base nesse buffer
+fronteira_terrestre <- st_intersection(brasil, buffer_fronteira)
+
+# Converter a fronteira terrestre em uma linha (apenas a borda do Brasil que faz fronteira terrestre)
+fronteira_linha <- st_boundary(fronteira_terrestre) |> 
+  group_by(COUNTRY) |> 
+  summarise()
+
+# Calcular a distância entre as sedes municipais e a linha da fronteira terrestre
+distancias_terrestres <- st_distance(sede_municipios, fronteira_linha)
+
+# Adicionar a distância calculada ao dataframe de sede_municipio
+sede_municipios$distancia_fronteira_terrestre <- as.numeric(distancias_terrestres)
+
+#### limite da faixa de fronteira. Definimos como fronteira interior o limite integral dos municípios pertencentes à FF
+# Esse limite é irregular, pois não acompanha os 150 km da fronteira terrestre.
+# Sedes municipais da atual faixa de fronteira (tratamento) tem distâncias negativas, enquanto o controle tem distâncias positivas.
+faixa <- st_read(file.path(dropbox,"Municipios_Fronteira/Municipios_Faixa_Fronteira_2022.shp")) %>%
+  st_transform("WGS84") |> 
+  mutate(pais = "BR") |>  
+  group_by(pais) |> 
+  summarise()
+
+faixa_linha <- st_boundary(faixa)
+
+# Criar um buffer de 5 km ao redor da linha de fronteira
+buffer_fronteira <- st_buffer(fronteira_linha, dist = 5000) |> 
+  st_union()
+
+# Definir a faixa de fronteira interior do Brasil como a exclusão desse buffer
+faixa_interior <- st_difference(faixa_linha, buffer_fronteira)
+
+# Calcular a distância entre as sedes municipais e a linha da fronteira terrestre
+distancias_interior <- st_distance(sede_municipios, faixa_interior)
+
+# Adicionar a distância calculada ao dataframe de sede_municipio
+sede_municipios$distancias_fronteira_interior <- as.numeric(distancias_interior)
+
+t <- sede_municipios |> 
+  select(id_municipio, distancia_fronteira_terrestre, distancias_fronteira_interior)
+st_geometry(t) <- NULL
+
+df <- df |> 
+  left_join(t, by = "id_municipio") |> 
+  #torna as distâncias da faixa de fronteira original negativas
+  mutate(distancias_fronteira_interior = ifelse(groups == "treatment", 
+                                                -distancias_fronteira_interior, 
+                                                distancias_fronteira_interior))
+
+
+rm(t)
+
+library(readr)
+write_rds(df, file.path(dropbox, "dados_espaciais.rds"))
---
+++ src/processamento_dados.R
@@ -0,0 +1,192 @@
+dropbox <- "c:/Users/victor/dropbox/DISSERTACAO"
+library(tidyverse)
+
+# Base do Sistema de Informações sobre Mortalidade (SIM)
+library(basedosdados)
+query <- bdplyr("basedosdados.br_ms_sim.municipio_causa")
+sim <- bd_collect(query)
+
+#### Analisando mortes violentas em 2019 #####
+# Vetor de prefixos para as causas violentas
+prefixos <- c("W32", "W33", "W34", "X85", "X86", "X87", "X88", "X89", "X90", 
+              "X91", "X92", "X93", "X94", "X95", "X96", "X97", "X98", "X99", 
+              "Y00", "Y01", "Y02", "Y03", "Y04", "Y05", "Y06", "Y07", "Y08", 
+              "Y09", "Y10", "Y11", "Y17", "Y18", "Y19", "Y20", "Y21", "Y22", 
+              "Y23", "Y24", "Y25", "Y26", "Y27", "Y28", "Y29", "Y30", "Y31", 
+              "Y32", "Y33", "Y34", "Y35", "Y87", "Y89", "Y12", "Y13", "Y14", 
+              "Y15", "Y16")
+
+library(dplyr)
+library(stringr)
+# Filtrando a tabela
+sim <- sim %>%
+  filter(str_detect(causa_basica, paste0("^", prefixos, collapse = "|"))) |> 
+  filter(ano == 2019) |> 
+  count(id_municipio, name = "mortes_violentas")
+
+df <- df |> 
+  left_join(sim, by="id_municipio")
+
+#Link base ministério segurança publica
+#https://www.gov.br/mj/pt-br/assuntos/sua-seguranca/seguranca-publica/estatistica/download/dnsp-base-de-dados/bancovde-2019.xlsx/@@download/file
+library(readxl)
+banco <- read_xlsx(file.path(dropbox, "BancoVDE 2019.xlsx"), col_types = c("text", "text", "text", "date", "text", "text", "text", "numeric", "numeric", "numeric", "numeric", "numeric", "text", "text", "numeric"))
+
+banco <- banco |> 
+  rename("nome" = "municipio",
+         "sigla_uf" = "uf")
+
+#### CRIAR BASES SEPARADAS PARA CADA EVENTO ####
+# Vamos criar uma lista com as diferentes categorias de eventos
+categorias_evento <- banco |> 
+  distinct(evento) |> 
+  pull()
+
+# Função para salvar cada categoria em um CSV
+salvar_csv <- function(evento) {
+  banco|> 
+    filter(evento == !!evento) |>  
+    write.csv(file = paste0("evento_", evento, ".csv"), row.names = FALSE)
+}
+
+# Aplicar a função em cada categoria
+library(purrr)
+walk(categorias_evento, salvar_csv)
+rm(banco)
+
+#VARIÁVEIS COM abrangencia estadual
+cocaina_table <- read_csv(file.path(dropbox, "evento_Apreensão de Cocaína.csv")) |> 
+  count(sigla_uf, nome, wt=total_peso, name = "cocaina")
+maconha_table <- read_csv(file.path(dropbox, "evento_Apreensão de Maconha.csv")) |> 
+  count(sigla_uf, nome, wt=total_peso, name = "maconha")
+apreensao_arma <- read_csv(file.path(dropbox, "evento_Arma de Fogo Apreendida.csv")) |> 
+  count(sigla_uf, nome, wt=total, name = "armas") 
+furto_veiculo <- read_csv(file.path(dropbox, "evento_Furto de veículo.csv")) |> 
+  count(sigla_uf, nome, wt=total, name = "furto_vei")
+roubo_banco_table <- read_csv(file.path(dropbox, "evento_Roubo a instituição financeira.csv")) |> 
+  count(sigla_uf, nome, wt=total, name = "rou_banco")
+roubo_carga_table <- read_csv(file.path(dropbox, "evento_Roubo de carga.csv")) |> 
+  count(sigla_uf, nome, wt=total, name = "rou_carga")
+roubo_veículo_table <- read_csv(file.path(dropbox, "evento_Roubo de veículo.csv")) |> 
+  count(sigla_uf, nome, wt=total, name = "rou_vei")
+trafico_table <- read_csv(file.path(dropbox, "evento_Tráfico de drogas.csv")) |> 
+  count(sigla_uf, nome, wt=total, name = "trafico")
+
+#VARIÁVEIS COM abrangencia municipal
+feminicidio_table <- read_csv(file.path(dropbox, "evento_Feminicídio.csv")) |> 
+  count(sigla_uf, nome, wt=total_vitimas, name = "feminicidio")
+hom_doloso_table <- read_csv(file.path(dropbox, "evento_Homicídio doloso.csv")) |> 
+  count(sigla_uf, nome, wt=total_vitimas, name = "hom_doloso")
+lesao_table <- read_csv(file.path(dropbox, "evento_Lesão corporal seguida de morte.csv")) |> 
+  count(sigla_uf, nome, wt=total_vitimas, name = "lesao")
+mandado_table <- read_csv(file.path(dropbox, "evento_Mandado de prisão cumprido.csv")) |> 
+  count(sigla_uf, nome, wt=total, name = "mandado")
+transito_table <- read_csv(file.path(dropbox, "evento_Morte no trânsito ou em decorrência dele (exceto homicídio doloso).csv")) |> 
+  count(sigla_uf, nome, wt=total_vitimas, name = "transito")
+esclarecer_table <- read_csv(file.path(dropbox, "evento_Mortes a esclarecer (sem indício de crime).csv")) |> 
+  count(sigla_uf, nome, wt=total_vitimas, name = "esclarecer")
+latrocinio_table <- read_csv(file.path(dropbox, "evento_Roubo seguido de morte (latrocínio).csv")) |> 
+  count(sigla_uf, nome, wt=total_vitimas, name = "latrocinio")
+tentativa_homicidio_table <- read_csv(file.path(dropbox, "evento_Tentativa de homicídio.csv")) |> 
+  count(sigla_uf, nome, wt=total_vitimas, name = "tentativa_hom")
+
+#tabela final
+municipal_table <- feminicidio_table |> 
+  left_join(hom_doloso_table, join_by(sigla_uf, nome)) |> 
+  left_join(lesao_table, join_by(sigla_uf, nome)) |> 
+  left_join(mandado_table, join_by(sigla_uf, nome)) |> 
+  left_join(transito_table, join_by(sigla_uf, nome)) |> 
+  left_join(esclarecer_table, join_by(sigla_uf, nome)) |> 
+  left_join(latrocinio_table, join_by(sigla_uf, nome)) |> 
+  left_join(tentativa_homicidio_table, join_by(sigla_uf, nome))
+
+rm(hom_doloso_table, lesao_table, mandado_table, transito_table, esclarecer_table, latrocinio_table, tentativa_homicidio_table)
+
+estadual_table <- cocaina_table |> 
+  left_join(maconha_table, join_by(sigla_uf, nome)) |> 
+  left_join(apreensao_arma, join_by(sigla_uf, nome)) |> 
+  left_join(furto_veiculo, join_by(sigla_uf, nome)) |> 
+  left_join(roubo_banco_table, join_by(sigla_uf, nome)) |> 
+  left_join(roubo_carga_table, join_by(sigla_uf, nome)) |> 
+  left_join(roubo_veículo_table, join_by(sigla_uf, nome)) |> 
+  left_join(trafico_table, join_by(sigla_uf, nome))
+
+rm(maconha_table, apreensao_arma, furto_veiculo, roubo_banco_table, roubo_carga_table, roubo_veículo_table, trafico_table)
+
+#### IMPORTANDO BASE PARA INSERIR OS CÓDIGOS IBGE ####
+library("basedosdados")
+# Defina o seu projeto no Google Cloud
+set_billing_id("rapid-pact-400813")
+# Para carregar o dado direto no R
+query <- bdplyr("br_bd_diretorios_brasil.municipio")
+codigos <- bd_collect(query)
+
+# Transformar os nomes dos municípios na tabela 'codigos' para maiúsculas
+codigos <- codigos %>%
+  mutate(nome_upper = toupper(nome))
+
+# Unir as tabelas com base no nome do município
+municipal_table <- municipal_table %>%
+  left_join(codigos, by = c("nome" = "nome_upper", "sigla_uf"))
+
+municipal_table <- municipal_table |> 
+  select(-nome, -(id_municipio_6:id_municipio_bcb), -(capital_uf:centroide)) |> 
+  rename("nome"="nome.y") |> 
+  relocate(id_municipio:nome, sigla_uf)
+
+municipal_table <- municipal_table |> 
+  left_join(sim, join_by(id_municipio))
+
+#### POPULACAO ####
+# Calcular em função da população
+query <- bdplyr("br_ibge_populacao.municipio")
+populacao <- bd_collect(query)
+
+municipal_table <- municipal_table |> 
+  left_join(populacao |>
+              filter(ano == 2019) |> 
+              select(id_municipio:populacao), join_by(id_municipio)) |> 
+  mutate(mortes_violentas = (mortes_violentas/populacao)*100000,
+         feminicidio = (feminicidio/populacao)*100000,
+         hom_doloso = (hom_doloso/populacao)*100000,
+         lesao = (lesao/populacao)*100000,
+         mandado = (mandado/populacao)*100000,
+         transito = (transito/populacao)*100000,
+         esclarecer = (esclarecer/populacao)*100000, 
+         latrocinio = (latrocinio/populacao)*100000,
+         tentativa_hom = (tentativa_hom/populacao)*100000) |> 
+  relocate(populacao, .before=feminicidio)
+
+homicidios <- read_excel(file.path(dropbox, "Base_homicidios.xlsx"))
+
+homicidios <- homicidios |> 
+  separate(Município, c("id_municipio_6", "nome_upper"), sep = 7) |> 
+  separate(id_municipio_6, c("id_municipio_6", NA), sep = -1) |> 
+  rename("taxa_analfabetismo" = "Taxa_de_analfabetismo",
+         "taxa_desemprego" = "Taxa_de_desemprego_16a_e+",
+         "gini" = "Gini",
+         "pibpc" = "PIB_per_capita",
+         "taxa_renda_pobre" = "%_população_com_renda_<_1/4_SM",
+         "taxa_trab_infantil" = "Taxa_de_trabalho_infantil",
+         "taxa_homens_jovens" = "Porcentagem_Homens_Jovens",
+         "valor_2010" = "valor-2010",
+         "valor_2011" = "valor-2011",
+         "valor_2012" = "valor-2012",
+         "valor_2013" = "valor-2013",
+         "valor_2014" = "valor-2014",
+         "valor_2015" = "valor-2015",
+         "valor_2016" = "valor-2016",
+         "valor_2017" = "valor-2017",
+         "valor_2018" = "valor-2018",
+         "valor_2019" = "valor-2019"
+         ) |> 
+  left_join(codigos |> select(id_municipio, nome, id_municipio_6), join_by(id_municipio_6)) |> 
+  relocate(id_municipio, nome) |> 
+  select(-id_municipio_6, -nome_upper)
+
+municipal_table <- municipal_table |> 
+  left_join(homicidios)
+
+#salvar
+write_rds(municipal_table, file.path(dropbox, "municipal.rds"))
+write_csv(estadual_table, file.path(dropbox,"estadual.csv"))
